7687cced18d15824ccce22a199df5392b9b11d28
paas10 <pablo.andre_alvarado@hotmail.com>
2019-09-30 02:05:13 -0600

    Analisis Sintactico
    Programación de las Gramáticas

---
+++ MiniSQL/src/minisql/TokenAnalisis.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minisql;
+
+/**
+ *
+ * @author Admin
+ */
+public class TokenAnalisis {
+    
+    public Token token;
+    public String produccion;
+    public int linea;
+    public int columna;
+    
+    public TokenAnalisis()
+    {
+        produccion = "";
+        linea = 0;
+        columna = 0;
+    }
+    
+    public TokenAnalisis(Token token, String produccion, int linea, int columna)
+    {
+        this.token = token;
+        this.produccion = produccion;
+        this.linea = linea;
+        this.columna = columna;
+    }
+    
+}
+
+
